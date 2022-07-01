7267751449a5abba0032d7882bcf7db46d9232b2
SJY0917032 <sea71511@gmail.com>
2022-07-01 09:41:56 +0900

    Fix : 쓰지않는 Redis 삭제

--- triple/src/app.module.ts
+++ triple/src/app.module.ts
@@ -2,10 +2,7 @@ import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//  redis
-import { RedisClientOptions } from 'redis';
-import * as redisStore from 'cache-manager-redis-store';
-//
+
 //Modules
 import { UserModule } from './apis/user/user.module';
 import { PlaceModule } from './apis/place/place.module';
@@ -34,11 +31,6 @@ import { EventModule } from './apis/event/event.module';
       logging: true, // 로그를 남긴다 (명령어가 어떻게 바뀌는지)
       retryAttempts: 30,
       retryDelay: 5000,
-    }),
-    CacheModule.register<RedisClientOptions>({
-      store: redisStore,
-      url: 'redis://triple-redis:6379',
-      isGlobal: true,
     }),
   ],
   controllers: [AppController],
